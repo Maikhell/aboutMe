cbdbca95e48c0259249ca8cdb64a63e269244d4c
John Michael Camara <johnmichael.camara.git@gmail.com>
2024-06-21 05:15:52 +0800

    First commit

---
+++ aboutme.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Me</title>
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/bootstrap-5.3.1-dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About Me</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Gallery</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-disabled="true">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-md-4 custom-border-bottom">
+                <img class="img-fluid" src="./img/Picsart_24-06-21_04-13-15-156.png" alt="Large Image">
+            </div>
+            <div class="col-md-8 d-flex align-items-center justify-content-center">
+                <h1 class="intro-title">
+                    Hi, I'm John Michael Camara
+                </h1>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+<script src="./css/bootstrap-5.3.1-dist/js/bootstrap.min.js"></script>
+
+</html>
